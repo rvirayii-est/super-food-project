727f9cb232a693d8994d99d8ff6644ccde89de7e
Kreutzer Hellscythe <elijahlancecatli@gmail.com>
2020-08-15 06:44:08 +0800

    Finished Register and Login Routes

---
+++ server/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	role: {
+		type: String,
+		default: "customer",
+		enum: ["customer", "rider", "store", "admin"],
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
